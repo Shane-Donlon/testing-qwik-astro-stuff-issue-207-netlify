6a2cf853c7c01e16d4fe23151c745c8700a94208
Shane-Donlon <shane.donlon@live.com>
2025-01-02 10:06:10 +0000

    intial commit

---
+++ astro.config.ts
@@ -0,0 +1,12 @@
+import qwikdev from "@qwikdev/astro";
+import { defineConfig } from "astro/config";
+
+
+import netlify from "@astrojs/netlify";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "server",
+  adapter: netlify(),
+  integrations: [qwikdev()],
+});
